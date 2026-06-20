351d9ded79622687a19ddc0e3c5d2abfaa4d1994
nale6 <nale6dev@gmail.com>
2026-06-20 00:06:45 -0400

    Pt2 of past commit: Undo button and some code cleanup

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -9,6 +9,7 @@ export default function ChessBoard() {
   const [board, setBoard] = useState(initBoard);
 
   //TODO: Chat, reevaluate over game and see points, visibly display what material you and opponent have taken, sidebar with profile, mock elo rating and system, change return/back button
+  //TODO: Timer, redo button, bugfix pawn (investigate more)
 
   return (
     <div className="min-h-screen bg-zinc-900 flex justify-center items-center p-4">
